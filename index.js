f8d8db432f85bfc6fe6cf01b59947e8dc2c0e0ab
pravesh5428 <pravesh@ubitechsolutions.com>
2020-08-27 03:37:20 +0530

    added index.js file

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-<!DOCTYPE HTML>
-<html>
 
-<body>
-
-  <p>Before the script...</p>
 
   <script>
     alert( 'Hello, world!' );
   </script>
 
-  <p>...After the script.</p>
 
-</body>
 
-</html>
+
